9cdcca6779dad3743654da2115f4789d209862cd
unvalid-user <natalya.kh2@mail.ru>
2025-10-14 00:56:35 +0300

    Encryptor fixed, refactored and tested

--- src/main/java/com/example/bankcards/service/UserService.java
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -4,10 +4,12 @@ import com.example.bankcards.entity.User;
 import com.example.bankcards.exception.ResourceNotFoundException;
 import com.example.bankcards.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import static com.example.bankcards.util.AppConst.PHONE_NUMBER;
 import static com.example.bankcards.util.AppConst.USER;
 
+@Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
